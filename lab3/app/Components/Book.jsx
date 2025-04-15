cb60e5c187a2b2ecf97851334075700130f198ce
DEJA <allowestq@gmail.com>
2025-04-15 21:20:45 +0200

    add edit/delete buttons

--- lab3/app/Components/Book.jsx
+++ lab3/app/Components/Book.jsx
@@ -1,13 +1,30 @@
-export default function Book({ book }) {
-  return (
-    <article className="list-horizontal">
-      <div className="book-info">
-        <div className="book-details">
-          <h3>{book.title}</h3>
-          <p className="author">{book.author}</p>
-          <p className="genre-text">{book.genre}</p>
-        </div>
-      </div>
-    </article>
-  );
+export default function Book({book}) {
+
+    const handleEdit = () => {
+
+    };
+
+    const handleDelete = () => {
+
+    };
+
+    return (
+        <article className="list-horizontal">
+            <div className="book-info">
+                <div className="book-details">
+                    <h3>{book.title}</h3>
+                    <p className="author">{book.author}</p>
+                    <p className="genre-text">{book.genre}</p>
+                </div>
+                <div className="book-actions">
+                    <button className="action-button edit-button" onClick={handleEdit}>
+                        Edytuj
+                    </button>
+                    <button className="action-button delete-button" onClick={handleDelete}>
+                        Usuń
+                    </button>
+                </div>
+            </div>
+        </article>
+    );
 } 
